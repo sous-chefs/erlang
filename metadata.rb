b122d9b80a38ca5b3504b97bbb80787d94516b85
jtimberman <joshua@opscode.com>
2012-11-13 22:57:36 -0700

    add name attribute and increment version for development

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,9 @@
+name             "erlang"
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs erlang, optionally install GUI tools."
-version           "1.1.0"
+version           "1.1.1"
 depends           "yum", ">= 0.5.0"
 
 recipe "erlang", "Installs erlang"
