44f87e0181f2e6cb4056e5841343d8a5b2b1f4f6
cmluciano <cmlucian@us.ibm.com>
2015-02-18 15:16:16 -0500

    when rhel, always include yum-epel

--- recipes/package.rb
+++ recipes/package.rb
@@ -38,10 +38,8 @@ when 'rhel'
       gpgcheck false
       action :create
     end
-  when 7
-    include_recipe 'yum-epel'
-    include_recipe 'yum-erlang_solutions'
   else
+    include_recipe 'yum-epel'
     include_recipe 'yum-erlang_solutions'
   end
 
