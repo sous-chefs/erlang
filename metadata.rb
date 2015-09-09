3d174ad5b2f0b8f53c207f84f3848f1ba9cdb0e3
Tim Smith <tsmith84@gmail.com>
2015-09-09 11:52:57 -0700

    Add long_description and update repo URLs

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache 2.0'
 description       'Installs erlang, optionally install GUI tools.'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '1.5.8'
 
 depends           'apt', '>= 1.7.0'
@@ -16,9 +17,9 @@ recipe 'erlang::package', 'Installs Erlang via native package'
 recipe 'erlang::source', 'Installs Erlang via source'
 recipe 'erlang::esl', "Installs Erlang from Erlang Solutions' package repositories"
 
-%w{ ubuntu debian redhat centos scientific amazon oracle }.each do |os|
+%w{ amazon centos debian oracle redhat scientific ubuntu }.each do |os|
   supports os
 end
 
-source_url 'https://github.com/opscode-cookbooks/erlang' if respond_to?(:source_url)
-issues_url 'https://github.com/opscode-cookbooks/erlang/issues' if respond_to?(:source_url)
+source_url 'https://github.com/chef-cookbooks/erlang' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/erlang/issues' if respond_to?(:issues_url)
