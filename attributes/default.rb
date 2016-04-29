5fc3b53acb32a68af200df7ddc67dd6304791916
Tim Smith <tsmith84@gmail.com>
2016-04-29 10:01:33 -0700

    Install 18.3 for source installs by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,6 +1,6 @@
 #
 # Author:: Joshua Timberman <joshua@chef.io>
-# Copyright:: Copyright (c) 2012, Chef Software, Inc. <legal@chef.io>
+# Copyright:: Copyright (c) 2012-2016, Chef Software, Inc. <legal@chef.io>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,8 +18,8 @@
 default['erlang']['gui_tools'] = false
 default['erlang']['install_method'] = 'package'
 
-default['erlang']['source']['version'] = 'R16B03'
-default['erlang']['source']['checksum'] = '6133b3410681a5c934e54c76eee1825f96dead8d6a12c31a64f6e160daf0bb06'
+default['erlang']['source']['version'] = '18.3'
+default['erlang']['source']['checksum'] = '0e24e32ca4912b7b08d6b8e33c25c107486da410f6c8d56ef81b17f018b0c614'
 default['erlang']['source']['build_flags'] = ''
 default['erlang']['source']['cflags'] = ''
 
