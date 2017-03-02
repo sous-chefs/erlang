0ed15f1859a1d9426c9cceb7b850ece0e6363d5e
Jennifer Davis <iennae@gmail.com>
2017-03-01 20:52:27 -0800

    Release 4.1.1 Signed-off-by: Jennifer Davis <iennae@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'cookbooks@chef.io'
 license           'Apache 2.0'
 description       'Installs erlang, optionally install GUI tools.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '4.1.0'
+version           '4.1.1'
 
 depends           'apt', '>= 1.7.0'
 depends           'yum', '>= 3.0'
