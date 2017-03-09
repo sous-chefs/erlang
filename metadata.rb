aae614b60d1379146be754768ba433fc81037e67
Seth Thomas <sthomas@chef.io>
2017-03-09 10:51:45 -0500

    Update deps for Chef 13
    Signed-off-by: Seth Thomas <sthomas@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,11 @@ maintainer_email  'cookbooks@chef.io'
 license           'Apache 2.0'
 description       'Installs erlang, optionally install GUI tools.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '4.1.1'
+version           '5.0.0'
 
-depends           'apt', '>= 1.7.0'
-depends           'yum', '>= 3.0'
+depends           'compat_resource'
 depends           'yum-epel'
-depends           'yum-erlang_solutions'
+depends           'yum-erlang_solutions', '>= 1.0.3'
 depends           'build-essential'
 
 recipe 'erlang', 'Installs Erlang via native package, source, or Erlang Solutions package'
