7dcf2d7567b5e9753c86736f1e5a17f2526453a2
Tim Smith <tsmith@chef.io>
2017-03-21 15:53:38 -0700

    Update apache2 license string
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name              'erlang'
 maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
-license           'Apache 2.0'
+license           'Apache-2.0'
 description       'Installs erlang, optionally install GUI tools.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '5.0.0'
